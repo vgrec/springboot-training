bbc97df00c49f595fc130fbc6db620941b5048c1
vgrec <grec.vs@gmail.com>
2020-11-01 16:13:54 +0100

    Add data classes

---
+++ review/src/main/java/com/udacity/jwdnd/data/Delivery.java
@@ -0,0 +1,33 @@
+package com.udacity.jwdnd.data;
+
+import java.sql.Timestamp;
+
+public class Delivery {
+    private int id;
+    private int orderId;
+    private Timestamp time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+}
---
+++ review/src/main/java/com/udacity/jwdnd/data/Order.java
@@ -0,0 +1,22 @@
+package com.udacity.jwdnd.data;
+
+public class Order {
+    private int id;
+    private int customerId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+}
